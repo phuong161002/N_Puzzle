bb68e7d536ecdea401757124b722b8419b965a16
VuThePhuong <thephuong161002@gmail.com>
2022-12-28 17:26:20 +0700

    Create new Data Structure : Priority Queue

---
+++ N_Puzzle/PriorityQueue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_Puzzle
+{
+    public class PriorityQueue<T>
+    {
+        private class PQNode
+        {
+            public int Priority;
+            public T Data;
+        }
+
+        private LinkedList<PQNode> DataList;
+
+        public PriorityQueue()
+        {
+            DataList = new LinkedList<PQNode>();
+        }
+
+        public void Enqueue(T Data, int priority)
+        {
+            PQNode newNode = new PQNode()
+            {
+                Data = Data,
+                Priority = priority
+            };
+
+
+            var current = DataList.First;
+            while (current != null && current.Value.Priority >= priority)
+            {
+                current = current.Next;
+            }
+
+            DataList.AddBefore(current, newNode);
+        }
+
+        public T Dequeue()
+        {
+            var node = DataList.First.Value;
+
+            DataList.RemoveFirst();
+
+            return node.Data;
+        }
+
+        public int Count
+        {
+            get { return DataList.Count; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return Count == 0; }
+        }
+    }
+}
